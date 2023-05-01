a38c006ce8fb59ae97aa62aa86d8e9bae71b2562
pipeeeeees <48690614+pipeeeeees@users.noreply.github.com>
2023-04-30 23:23:31 -0400

    untested car detection

--- src/main.py
+++ src/main.py
@@ -1,10 +1,20 @@
 import cv2
+import paths
 
+car_cascade = cv2.CascadeClassifier(paths.CASCADE_PATH)
+
+# set the camera for cv2 to capture from (index 0 as I don't use more than one USB camera)
 cap = cv2.VideoCapture(0)
 
 def main():
     while True:
         ret, frame = cap.read()
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cars = car_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+        for (x, y, w, h) in cars:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
         
         # perform operations on the frame (e.g., display, process, etc.)
         cv2.imshow('Webcam Feed', frame)
